41cad00a7ebfec56083cbfa97b391e3e11ba9c47
Anusuma Choudhary <107402293+CoderChori@users.noreply.github.com>
2023-12-12 16:37:35 +0530

    Create age_left_calculator.py

---
+++ age_left_calculator.py
@@ -0,0 +1,5 @@
+age = int(input("enter your age: "))
+days = (90*365) - (age*365)
+weeks = (90*52) - (age*52)
+months = (90*12) - (age*12)
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
